3bd58a1ac501075ee88069d60f512028530a9d71
Николай <123198970+IFilkov@users.noreply.github.com>
2024-07-23 12:28:41 +0300

    Update test_01.js

--- test_01.js
+++ test_01.js
@@ -30,7 +30,7 @@ function preload() {
   this.load.image("bomb", "assets/bomb.png");
   this.load.spritesheet("dude", "assets/dude.png", {
     frameWidth: 32,
-    frameHeight: 48,
+    frameHeight: 33,
   });
 }
 
